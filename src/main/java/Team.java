1f4628662b789d71c4ff66c45984e84b49bbbc51
Xi Xia <triplextension@gmail.com>
2017-03-24 08:10:33 -0700

    team object created

--- src/main/java/Team.java
+++ src/main/java/Team.java
@@ -1,3 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Team {
+  private String mTeamName;
+
+  public Team (String name) {
+    mTeamName = name;
+  }
 
 }
--- src/test/java/TeamTest.java
+++ src/test/java/TeamTest.java
@@ -2,5 +2,10 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class TeamTest {
+  @Test
+  public void createTeamInstance_true (){
+    Team newTeam = new Team ("Kodiak");
+    assertTrue(newTeam instanceof Team);
+  }
 
 }
