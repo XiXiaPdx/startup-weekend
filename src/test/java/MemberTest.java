5184d16da92ffe7df40fc48c256dd7885e47ac9b
Xi Xia <triplextension@gmail.com>
2017-03-24 09:06:10 -0700

    add member instance creation

---
+++ src/test/java/MemberTest.java
@@ -0,0 +1,20 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class MemberTest {
+    private Member newMember;
+
+    @Before
+    public void createNewTeamAutoAndClear(){
+    //  Team.clearTeamInstances();
+     newMember = new Member ("Sera");
+    }
+
+    @Test
+    public void createMemberInstance_true (){
+      assertTrue(newMember instanceof Member);
+    }
+
+
+
+}
