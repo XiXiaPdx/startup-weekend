28428b2ca5d98f3b45c2ba040973d94f573b29b4
Xi Xia <triplextension@gmail.com>
2017-03-24 08:12:24 -0700

    add @Before for Team test. Team object auto now

--- src/test/java/TeamTest.java
+++ src/test/java/TeamTest.java
@@ -2,9 +2,15 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class TeamTest {
+  private Team newTeam;
+
+  @Before
+  public void createNewTeamAuto(){
+   newTeam = new Team ("Kodiak");
+  }
+
   @Test
   public void createTeamInstance_true (){
-    Team newTeam = new Team ("Kodiak");
     assertTrue(newTeam instanceof Team);
   }
 
